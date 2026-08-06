b19ac6104d7586deecb6b79cb20ae565e07950f0
Sandeep Dixit <dixitsandeep339@gmail.com>
2026-08-06 09:18:54 +0530

    Restructure: move ResumeIQ into own subfolder, remove unrelated projects

--- dpiic-backend/run_server.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-start "" cmd /c "timeout /t 5 /nobreak >nul & start http://127.0.0.1:8000/"
-"C:\Users\Ats\AppData\Local\Python\bin\python.exe" -m uvicorn app.main:app --host 127.0.0.1 --port 8000
